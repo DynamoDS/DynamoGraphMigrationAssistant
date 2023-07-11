711da9ad294fce39f89e7677c48cd708826791d3
John <sixtysecondrevit@gmail.com>
2023-07-11 16:24:24 -0600

    Alert user to click file trust.

--- DynamoGraphMigrationAssistantViewExtension/Views/GraphMigrationAssistantView.xaml.cs
+++ DynamoGraphMigrationAssistantViewExtension/Views/GraphMigrationAssistantView.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -66,6 +67,11 @@ namespace DynamoGraphMigrationAssistant.Views
             GraphMigrationAssistantViewModel vm = this.DataContext as GraphMigrationAssistantViewModel;
 
             vm.EditGraphSettingsCommand.Execute(this);
+        }
+
+        private void ExportButton_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+           
         }
     }
 }
